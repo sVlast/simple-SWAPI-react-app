831216b8742b214c0509cbf878aedb6d4e46dbf7
Sandro Vlastelica <sandro.vlastelica@hotmail.com>
2021-05-29 11:11:55 +0200

    updateed to yarn 2 and added eslint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "antd/dist/antd.css";
+import { List, Card } from "antd";
+
+//const fetchData = await fetch("https://swapi.dev/api/people/1");
+
+const data = [
+    {
+        title: "Dizajn digitalnih proizvoda",
+        startTime: 9,
+        endTime: 10,
+        type: "Predavanje",
+    },
+    {
+        title: "Digitalna fotografija",
+        startTime: 14,
+        endTime: 15,
+        type: "Laboratorijske vježbe",
+    },
+    {
+        title: "Digitalna slika",
+        startTime: 16,
+        endTime: 17,
+        type: "Laboratorijske vježbe",
+    },
+    {
+        title: "XML programiranje",
+        startTime: 18,
+        endTime: 19,
+        type: "Predavanje",
+    },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header>
+                <h1>Sample text</h1>
+            </header>
+            <List
+                grid={{ gutter: 16, column: 3 }}
+                dataSource={data}
+                renderItem={(item) => (
+                    <List.Item>
+                        <Card title={item.title}>
+                            <span>{item.type}</span>
+                            <span>{`${item.startTime} - ${item.endTime}`}</span>
+                        </Card>
+                    </List.Item>
+                )}
+            />
+            <footer>
+                <p>(c) Sandro Vlastelica - 2021</p>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
