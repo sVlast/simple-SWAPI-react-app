7fad4de0614f4fcee49165bcfcf62441f6b3cd70
Sandro Vlastelica <sandro.vlastelica@hotmail.com>
2021-05-29 13:54:28 +0200

    Made some stuff happen

--- src/App.js
+++ src/App.js
@@ -1,50 +1,93 @@
 import "./App.css";
 import "antd/dist/antd.css";
-import { List, Card } from "antd";
+import { List, Card, Button } from "antd";
+import React, { useState, useEffect } from "react";
 
 //const fetchData = await fetch("https://swapi.dev/api/people/1");
 
-const data = [
-    {
-        title: "Dizajn digitalnih proizvoda",
-        startTime: 9,
-        endTime: 10,
-        type: "Predavanje",
-    },
-    {
-        title: "Digitalna fotografija",
-        startTime: 14,
-        endTime: 15,
-        type: "Laboratorijske vježbe",
-    },
-    {
-        title: "Digitalna slika",
-        startTime: 16,
-        endTime: 17,
-        type: "Laboratorijske vježbe",
-    },
-    {
-        title: "XML programiranje",
-        startTime: 18,
-        endTime: 19,
-        type: "Predavanje",
-    },
-];
-
 function App() {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch("https://swapi.dev/api/people/1")
+            .then((response) => {
+                if (response.ok) {
+                    const resp = response.json();
+                    console.log(resp);
+                    return resp;
+                }
+                throw response;
+            })
+            .then((data) => {
+                console.log("dataB: ", data);
+                //setData((data) => [...data, `${data.length}`]);
+                setData([data]);
+                console.log("dataA: ", data);
+            })
+            .catch((error) => {
+                console.error("Error fetching data: ", error);
+                setError(error);
+            })
+            .finally(() => setLoading(false));
+    }, []);
+
+    if (loading) return "Loading...";
+    if (error) return "Error!";
+
+    function getChara() {
+        let number = Math.round(Math.random() * (82 - 1) + 1);
+        console.log("rand: ", number);
+        fetch("https://swapi.dev/api/people/" + number)
+            .then((response) => {
+                if (response.ok) {
+                    const resp = response.json();
+                    console.log(resp);
+                    return resp;
+                }
+                throw response;
+            })
+            .then((respData) => {
+                console.log("dataB: ", respData);
+                //setData((data) => [...data, `${data.length}`]);
+                setData([...data, respData]);
+                console.log("dataA: ", respData);
+            })
+            .catch((error) => {
+                console.error("Error fetching data: ", error);
+                setError(error);
+            })
+            .finally(() => setLoading(false));
+    }
+
+    function clearChara() {
+        setData([]);
+    }
+
     return (
         <div className="App">
             <header>
-                <h1>Sample text</h1>
+                <h1>StarWars Charachters</h1>
             </header>
+            <div>
+                <Button onClick={getChara}>Get Random Character</Button>
+                <Button onClick={clearChara}>Clear</Button>
+            </div>
+
             <List
                 grid={{ gutter: 16, column: 3 }}
                 dataSource={data}
                 renderItem={(item) => (
                     <List.Item>
-                        <Card title={item.title}>
-                            <span>{item.type}</span>
-                            <span>{`${item.startTime} - ${item.endTime}`}</span>
+                        <Card title={item.name}>
+                            {console.log("datasource: ", data)}
+                            <span>Birth Year: {item.birth_year}</span>
+                            <br />
+                            <span>Height: {item.height}</span>
+                            <br />
+                            <span>Weight: {item.mass}</span>
+                            {/* <span>{`${item.startTime} - ${item.endTime}`}</span> */}
                         </Card>
                     </List.Item>
                 )}
